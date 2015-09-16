5a45ae5ed15f137f22ccad6e0986a351e41b369c
creightonja <creightonja@gmail.com>
2015-09-16 12:32:54 -0700

    add gameplay function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 var winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-//var gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var gameBoard = [9, 9, 9, 9, 9, 9, 9, 9, 9];
 
 
 
 
 function takeTurn(playerId, boardNum) {
-  if (gameBoard[boardNum] == 0){
-    $("." + boardNum).text(playerId);
+  if (gameBoard[boardNum] == 9){
     gameBoard[boardNum] = playerId;
   } else {
     return false;
   }
+  return gameBoard;
 };
 
 
@@ -18,10 +18,9 @@ function findWinner(gameBoard) {
   for (var i = 0; i < 8; i++){
     var winCombos = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     var checkWinner = gameBoard[winCombos[i][0]] + gameBoard[winCombos[i][1]] + gameBoard[winCombos[i][2]];
-    console.log(checkWinner);
-    if (checkWinner == "XXX"){
+    if (checkWinner == 0){
       return "Player 1 Wins!";
-    } else if (checkWinner == "OOO") {
+    } else if (checkWinner == 3) {
       return "Player 2 Wins!";
     } else {
       var winner = 0;
--- spec/specs.js
+++ spec/specs.js
@@ -1,44 +1,85 @@
-
 describe('findWinner', function () {
   it("Expects X in position 0,1,2 to equal Player 1 Wins", function() {
-    expect(findWinner(["X", "X", "X", 0, 0, 0, 0, 0, 0])).to.equal("Player 1 Wins!");
+    expect(findWinner([0, 0, 0, 9, 9, 9, 9, 9, 9])).to.equal("Player 1 Wins!");
   });
 
   it("Expects X in position 3,4,5 to equal Player 1 Wins", function() {
-    expect(findWinner([0, 0, 0, "X", "X", "X", 0, 0, 0])).to.equal("Player 1 Wins!");
+    expect(findWinner([9, 9, 9, 0, 0, 0, 9, 9, 9])).to.equal("Player 1 Wins!");
   });
 
   it("Expects X in position 7,8,9 to equal Player 1 Wins", function() {
-    expect(findWinner([0, 0, 0, 0, 0, 0, "X", "X", "X"])).to.equal("Player 1 Wins!");
+    expect(findWinner([9, 9, 9, 9, 9, 9, 0, 0, 0])).to.equal("Player 1 Wins!");
   });
 
 
   it("Expects X in position 0,3,6 to equal Player 1 Wins", function() {
-    expect(findWinner(["X", 0, 0, "X", 0, 0, "X", 0, 0])).to.equal("Player 1 Wins!");
+    expect(findWinner([0, 9, 9, 0, 9, 9, 0, 9, 9])).to.equal("Player 1 Wins!");
   });
 
   it("Expects X in position 1,4,7 to equal Player 1 Wins", function() {
-    expect(findWinner([0, "X", 0, 0, "X", 0, 0, "X", 0])).to.equal("Player 1 Wins!");
+    expect(findWinner([9, 0, 9, 9, 0, 9, 9, 0, 9])).to.equal("Player 1 Wins!");
   });
 
   it("Expects X in position 2,5,8 to equal Player 1 Wins", function() {
-    expect(findWinner([0, 0, "X", 0, 0, "X", 0, 0, "X"])).to.equal("Player 1 Wins!");
+    expect(findWinner([9, 9, 0, 9, 9, 0, 9, 9, 0])).to.equal("Player 1 Wins!");
   });
 
   it("Expects X in position 0,4,8 to equal Player 1 Wins", function() {
-    expect(findWinner(["X", 0, 0, 0, "X", 0, 0, 0, "X"])).to.equal("Player 1 Wins!");
+    expect(findWinner([0, 9, 9, 9, 0, 9, 9, 9, 0])).to.equal("Player 1 Wins!");
   });
 
   it("Expects X in position 2,4,6 to equal Player 1 Wins", function() {
-    expect(findWinner([0, 0, "X", 0, "X", 0, "X", 0, 0])).to.equal("Player 1 Wins!");
+    expect(findWinner([9, 9, 0, 9, 0, 9, 0, 9, 9])).to.equal("Player 1 Wins!");
   });
 
-  it("Expects O in position 0,1,2 to equal Player 2 Wins", function() {
-    expect(findWinner(["O", "O", "O", 0, 0, 0, 0, 0, 0])).to.equal("Player 2 Wins!");
+  it("Expects O in position 0,1,2 to equal Player 9 Wins", function() {
+    expect(findWinner([1, 1, 1, 9, 9, 9, 9, 9, 9])).to.equal("Player 2 Wins!");
   });
 
   it("Expects non-winning combo to equal 0", function() {
-    expect(findWinner(["O", "O", 0, 0, 0, 0, 0, 0, 0])).to.equal(0);
+    expect(findWinner([1, 1, 9, 9, 9, 9, 9, 9, 9])).to.equal(0);
+  });
+});
+
+
+describe('takeTurn', function () {
+  it("Expects Player 1 marking position 0 to change gameBoard", function() {
+    expect(takeTurn(0, 0)).to.eql([0, 9, 9, 9, 9, 9, 9, 9, 9]);
   });
 
+  it("Expects Player 2 marking position 4 to change gameBoard", function() {
+    expect(takeTurn(1, 4)).to.eql([0, 9, 9, 9, 1, 9, 9, 9, 9]);
+  });
+
+  it("Expects Player 1 marking position 3 to change gameBoard", function() {
+    expect(takeTurn(0, 3)).to.eql([0, 9, 9, 0, 1, 9, 9, 9, 9]);
+  });
+
+  it("Expects Player 2 marking position 6 to change gameBoard", function() {
+    expect(takeTurn(1, 6)).to.eql([0, 9, 9, 0, 1, 9, 1, 9, 9]);
+  });
+
+  it("Expects Player 1 marking position 1 to change gameBoard", function() {
+    expect(takeTurn(0, 1)).to.eql([0, 0, 9, 0, 1, 9, 1, 9, 9]);
+  });
+
+  it("Expects Player 2 marking position 2 to change gameBoard", function() {
+    expect(takeTurn(1, 2)).to.eql([0, 0, 1, 0, 1, 9, 1, 9, 9]);
+  });
+
+  it("Expects Player 1 marking position 5 to change gameBoard", function() {
+    expect(takeTurn(0, 5)).to.eql([0, 0, 1, 0, 1, 0, 1, 9, 9]);
+  });
+
+  it("Expects Player 2 marking position 7 to change gameBoard", function() {
+    expect(takeTurn(1, 7)).to.eql([0, 0, 1, 0, 1, 0, 1, 1, 9]);
+  });
+
+  it("Expects Player 1 marking position 8 to change gameBoard", function() {
+    expect(takeTurn(0, 8)).to.eql([0, 0, 1, 0, 1, 0, 1, 1, 0]);
+  });
+
+  it("Expects an already marked spot to return false", function() {
+    expect(takeTurn(1, 7)).to.eql(false);
+  });
 });
